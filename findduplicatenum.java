0e501369c78a0dfebaba66d91bb113170f0b016e
Sindhuuu04 <145040473+Sindhuuu04@users.noreply.github.com>
2024-07-17 01:25:56 +0530

    Update findduplicatenum.java

--- findduplicatenum.java
+++ findduplicatenum.java
@@ -1,21 +1,20 @@
-public class findduplicatenum {
+class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[nums[0]];
-        int fast = nums[nums[nums[0]]];
+      int slow = nums[nums[0]];
+    int fast = nums[nums[nums[0]]];
 
-        // Phase 1: Find intersection point in the cycle
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    }
 
-        // Phase 2: Find the entrance to the cycle
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
+    slow = nums[0];
 
-        return slow;
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+
+    return slow;  
     }
 }
